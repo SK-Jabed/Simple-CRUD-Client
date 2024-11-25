92631a70f42b2f3c5638828b10f7ab2e38b7d326
Sheikh Jabed <jabedbd2295@gmail.com>
2024-11-25 22:51:57 +0600

    Users Delete and Update Activity Completed

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,8 +1,10 @@
-import { useLoaderData } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 
 const Users = () => {
-    const users = useLoaderData();
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
 
     const handleDeleteUser = (_id) => {
         console.log("Delete:", _id);
@@ -14,18 +16,26 @@ const Users = () => {
                 console.log(data);
                 if (data.deletedCount > 0) {
                     alert("Deleted Successfully");
+                    const remaining = users.filter(user => user._id !== _id);
+                    setUsers(remaining);
                 }
         })
     }
     return (
+      <div>
+        Users: {users.length}
         <div>
-            Users: {users.length}
-            <div>
-                {
-                    users.map(user => <p key={user._id}>{user._id}: {user.name}, {user.email} <button onClick={() => handleDeleteUser(user._id)}>X</button></p>)
-                }
-            </div>
+          {users.map((user) => (
+            <p key={user._id}>
+              {user._id}: {user.name}, {user.email}{" "}
+              <Link to={`/update/${user._id}`}>
+                <button>Update</button>
+              </Link>
+              <button onClick={() => handleDeleteUser(user._id)}>X</button>
+            </p>
+          ))}
         </div>
+      </div>
     );
 };
 
