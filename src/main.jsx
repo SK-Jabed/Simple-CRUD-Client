948a4e7175a8048ac868cca7768ef7f99db49835
Sheikh Jabed <jabedbd2295@gmail.com>
2025-02-19 23:56:24 +0600

    Main JSX File Updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Users from './components/Users.jsx';
-import Update from './components/Update.jsx';
+import Users from "./components/Users.jsx";
+import Update from "./components/Update.jsx";
+
+
 
 const router = createBrowserRouter([
   {
@@ -19,8 +21,8 @@ const router = createBrowserRouter([
   {
     path: "/update/:id",
     element: <Update></Update>,
-    loader: ({params}) => fetch(`http://localhost:5000/users/${params.id}`)
-  }
+    loader: ({ params }) => fetch(`http://localhost:5000/users/${params.id}`),
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
